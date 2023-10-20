8328415c811c7c662e7cacab2bdb49d0d21331eb
Ranko Nikolic <ranko.nikoli@bcility.com>
2023-10-20 23:58:11 +0200

    Added mapping

---
+++ ComtradeProject/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ComtradeProject.DTOs;
+using ComtradeProject.Model;
+using ServiceReference;
+
+namespace ComtradeProject.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        { 
+            CreateMap<Agent, RegisterResponseDTO>().ReverseMap();
+            CreateMap<Agent, RegisterDTO>().ReverseMap();
+
+            CreateMap<Model.Person, ServiceReference.Person>().ReverseMap();
+            CreateMap<Model.Person, PersonDTO>().ReverseMap();
+            
+        }
+    }
+}
--- ComtradeProject/Program.cs
+++ ComtradeProject/Program.cs
@@ -3,39 +3,62 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using ComtradeProject.Database;
 using System.Text;
+using ServiceReference;
+using ComtradeProject.Database;
 using ComtradeProject.Repository;
+using ComtradeProject.Services;
+using ComtradeProject.Mapping;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DBContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectContext"));
-});
-
-builder.Services.AddScoped<IAgentRepository, AgentRepository>();
-builder.Services.AddScoped<IPersonRepository, PersonRepository>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace ComtradeProject
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DBContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectContext"));
+            });
+
+            builder.Services.AddScoped<IAgentRepository, AgentRepository>();
+            builder.Services.AddScoped<IAgentService, AgentService>();
+
+            builder.Services.AddScoped<IPersonRepository, PersonRepository>();
+
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+            IMapper mapper = mapperConfig.CreateMapper();
+            builder.Services.AddSingleton(mapper);
+
+            builder.Services.AddTransient<SOAPDemoSoapClient>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
